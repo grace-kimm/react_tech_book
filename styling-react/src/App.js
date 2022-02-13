22c3279295497019847f4d02d11619f5a4d16cea
grace.all <jailke93@gmail.com>
2022-02-13 22:50:16 +0900

    styled-components

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
-import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 function App() {
-  return <SassComponent />;
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  )
 }
 
 export default App;
