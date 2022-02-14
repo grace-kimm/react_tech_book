7eec4e68392837f394a37fcdae47fbdbfcf257c0
grace.all <jailke93@gmail.com>
2022-02-14 18:36:48 +0900

    fininsh to-do app (#10) & start immer-tutorial

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,57 +1,59 @@
 import TodoTemplate from './components/TodoTemplate.js';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { useState, useRef, useCallback } from 'react';
+import { useRef, useCallback, useState } from 'react';
+
+function createBulkTodos() {
+  const array = [];
+  for (let i=1; i<=2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false
+    })
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo)
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id)
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      )
+    default:
+      return todos
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: '리액트의 기초 알아보기',
-      checked: true
-    },
-    {
-      id:2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true
-    },
-    {
-      id:3,
-      text: '일정 관리 앱 만들어보기',
-      checked: false
-    }
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고유값으로 사용 될 id
   // ref를 사용해서 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
+  const onInsert = useCallback(text => {
       const todo = {
         id: nextId.current,
         text,
         checked: false
       }
-      setTodos(todos.concat(todo));
-      nextId.current += 1; // nextId 1씩 더하기
-    },
-    [todos]
-  )
+      setTodos(todos => todos.concat(todo))
+      nextId.current += 1 // nextId 1씩 더하기
+    }, [])
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    }, [todos]
-  )
+  const onRemove = useCallback(id => {
+      setTodos(todos => todos.filter(todo => todo.id !== id))
+    }, [])
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      ))
-    }, [todos]
-  )
+  const onToggle = useCallback(id => {
+      setTodos(todos => todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo))
+    }, [])
 
   return (
     <TodoTemplate>
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,7 +1,17 @@
 import TodoListItem from './TodoListItem';
 import './TodoListItem.scss';
+import React, { useCallback } from 'react';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+  const rowRenderer = useCallback(
+    ({index, key, style}) => {
+    const todo = todos[index];
+    return (
+      <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style}/>
+    )
+  }, [onRemove, onToggle, todos]
+)
+
   return (
     <div className='TodoList'>
       {todos.map(todo => (
@@ -11,4 +21,6 @@ const TodoList = ({todos, onRemove, onToggle}) => {
   )
 }
 
-export default TodoList;
+
+
+export default React.memo(TodoList);
